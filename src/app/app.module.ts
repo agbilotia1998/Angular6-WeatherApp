ce6ec3b5d92f779aa89644e362fc71dc6200dd2d
agbilotia1998 <iit2016508@iiita.ac.in>
2018-12-10 14:00:28 +0530

    Adds docs directory for deployment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InputLocationComponent } from './input-location/input-location.component';
 import { WeatherDataComponent } from './weather-data/weather-data.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,8 @@ import { WeatherDataComponent } from './weather-data/weather-data.component';
     GooglePlaceModule,
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
